7811734278da133fed5adc3bc285f2a6407a1b6c
Tyler Stevens <trstevens@UCSD.EDU>
2022-10-21 14:03:05 -0700

    added AboutPage route

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -10,31 +10,6 @@ public partial class AppShell : Shell
 	{
 		InitializeComponent();
 
-        LiveCharts.Configure(config =>
-        config
-            // registers SkiaSharp as the library backend
-            // REQUIRED unless you build your own
-            .AddSkiaSharp()
-
-            // adds the default supported types
-            // OPTIONAL but highly recommend
-            .AddDefaultMappers()
-
-            // select a theme, default is Light
-            // OPTIONAL
-            //.AddDarkTheme()
-            .AddLightTheme()
-
-            // finally register your own mappers
-            // you can learn more about mappers at:
-            // ToDo add website link...
-            //.HasMap<City>((city, point) =>
-            //{
-            //    point.PrimaryValue = city.Population;
-            //    point.SecondaryValue = point.Context.Index;
-            //})
-        // .HasMap<Foo>( .... )
-        // .HasMap<Bar>( .... )
-        );
+        Routing.RegisterRoute(nameof(AboutPage), typeof(AboutPage));
     }
 }
